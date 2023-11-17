243ea34adeeedaa9a6f2d929b9464b22de94775e
Caden Cardell <cadencardell@gmail.com>
2023-11-17 17:28:54 -0500

    working data manipulation

--- parse_sensor_data.py
+++ parse_sensor_data.py
@@ -2,11 +2,24 @@ import torch
 import numpy as np
 import re
 import math
+import matplotlib.pyplot as plt
 
 
-def load_data_from_csv(input_filepath, output_filepath):
-    x_window_size = 120
-    y_window_size = 24
+def get_sample_indices(data_length, x_window_size=120, y_window_size=24):
+
+    sample_length = data_length - x_window_size - y_window_size
+
+    # create feature indices
+    feature_indices = torch.arange(x_window_size).view(1, -1) + torch.arange(sample_length).view(-1, 1)
+
+    # create label indices
+    label_indices = torch.arange(y_window_size).view(1, -1) + torch.arange(sample_length).view(-1, 1)
+    label_indices = torch.add(label_indices, x_window_size)
+
+    return feature_indices, label_indices
+
+
+def load_data_from_csv(input_filepath):
 
     def extract_last_float(line):
         # Extract all float numbers from the line
@@ -19,87 +32,128 @@ def load_data_from_csv(input_filepath, output_filepath):
         for line in file:
             last_float = float(extract_last_float(line))
             data.append(last_float)
-    data = np.array(data)
 
-    data_length = len(data) - x_window_size - y_window_size
-
-    # Create indices for extracting windows
-    indices = torch.arange(x_window_size).view(1, -1) + torch.arange(data_length).view(-1, 1)
-
-    # Use advanced indexing to create train_x
-    all_x = data[indices]
-
-    # Create indices y
-    indices = torch.arange(y_window_size).view(1, -1) + torch.arange(data_length).view(-1, 1)
-    indices = torch.add(indices, x_window_size)
-
-    # Use advanced indexing to create train_x
-    all_y = data[indices]
-
-    np.savez(output_filepath,
-             all_x=all_x[:, :, np.newaxis],  # add dimension because this model works with multi-feature data
-             all_y=all_y)
+    return data
 
 
-def synthesize_predictions(input_filepath='real_data/synthesized_data.npz'):
-    """
-        IMPORT REAL DATA
-    """
-    input_data = np.load(input_filepath)
-
-    # separate data
-    all_x = input_data['all_x']
-    all_y = input_data['all_y']
-
-    # get important dimensions of data
-    x_window_size = all_x.shape[1]
-    y_window_size = all_y.shape[1]
-    number_of_points = len(all_y) + x_window_size + y_window_size
-
-    # create y indices
-    indices = torch.arange(y_window_size).view(1, -1) + torch.arange(number_of_points).view(-1, 1)
-    indices = torch.add(indices, x_window_size)
-
-    """
-        SYNTHESIS PREDICTION DATA
-    """
+def synthesize_predictions_from_data(input_filepath):
+    # import data
+    data = np.array(load_data_from_csv(input_filepath))
+    data_length = len(data)
 
     # generate prediction error
-    start_mean, end_mean = 0, 5
-    start_std, end_std = 1, 3
+    start_mean, end_mean = 0, 0
+    start_std, end_std = .1, .1
 
     # Create tensors for linearly interpolated mean and standard deviation
-    means = torch.linspace(start_mean, end_mean, number_of_points)
-    stds = torch.linspace(start_std, end_std, number_of_points)
+    means = torch.linspace(start_mean, end_mean, data_length)
+    stds = torch.linspace(start_std, end_std, data_length)
 
     # Sample from the normal distribution for each index
     dist_1_prediction_deltas = torch.stack([torch.normal(mean, std) for mean, std in zip(means, stds)])
 
     # Define parameters for the sine wave
-    amplitude = 3  # Amplitude of the sine wave
-    frequency = 2 * math.pi / number_of_points  # Frequency - one full cycle over the number of points
+    amplitude = 1  # Amplitude of the sine wave
+    frequency = 14 * math.pi / data_length  # Frequency - one full cycle over the number of points
 
     # Create a tensor for the x values
-    x_values = torch.linspace(0, number_of_points - 1, number_of_points)
+    x_values = torch.linspace(0, data_length - 1, data_length)
 
     # Calculate the sine wave for the means
     means = amplitude * torch.sin(frequency * x_values)
 
     # Define standard deviation range
-    start_std, end_std = 4, 1
+    start_std, end_std = .1, .1
 
     # Create tensors for linearly interpolated standard deviation
-    stds = torch.linspace(start_std, end_std, number_of_points)
+    stds = torch.linspace(start_std, end_std, data_length)
 
     # Sample from the normal distribution for each index
     dist_2_prediction_deltas = torch.stack([torch.normal(mean, std) for mean, std in zip(means, stds)])
 
-    dist_1_pred = dist_1_prediction_deltas[indices] + all_y
-    dist_2_pred = dist_2_prediction_deltas[indices] + all_y
+    dist_1_prediction = dist_1_prediction_deltas + data
+    dist_2_prediction = dist_2_prediction_deltas + data
+
+    return np.column_stack((dist_1_prediction, dist_2_prediction))
 
 
+def create_samples_from_data(input_filepath, output_filepath, x_window_size=120, y_window_size=24):
+    # load data
+    data = np.array(load_data_from_csv(input_filepath))
+
+    # calculate number of data points
+    data_length = len(data)
+
+    # create indices
+    feature_indices, label_indices = get_sample_indices(data_length)
+
+    # use advanced indexing to create features
+    all_x = data[feature_indices]
+
+    # use advanced indexing to create labels
+    all_y = data[label_indices]
+
+    # save to output file
+    np.savez(output_filepath,
+             all_x=all_x[:, :, np.newaxis],  # add dimension because this model works with multi-feature data
+             all_y=all_y)
+
+
+def create_prediction_samples_from_data(input_filepath):
+    # get base model predictions
+    base_model_predictions = synthesize_predictions_from_data(input_filepath)
+
+    # get important dimensions
+    number_of_base_models = base_model_predictions.shape[1]  # number of base models
+    data_length = len(base_model_predictions)
+
+    # get the indices for training samples
+    _, label_indices = get_sample_indices(data_length)
+
+    # get samples from base models
+    merge_base_models_samples = []
+    for base_model_num in range(number_of_base_models):
+        bm = base_model_predictions[:, base_model_num]
+        bm = bm[label_indices]
+        bm = np.expand_dims(bm, axis=1)
+        merge_base_models_samples.append(bm)
+    base_model_samples = np.concatenate(merge_base_models_samples, axis=1)
+
+    return base_model_samples
+
+
+def plot_data_vs_predictions(data, base_model_predictions):
+
+    # plotting
+    n = base_model_predictions.shape[1]  # number of base models
+    for base_model_num in range(n):
+        plt.plot(base_model_predictions[:, base_model_num], label=f"base model {base_model_num}")
+    plt.plot(data, label="data", color='red')
+
+    # labeling
+    plt.xlabel('15 minute time steps')
+    plt.ylabel('Degrees F')
+    plt.title('Synthetic Model Predictions')
+    plt.legend()
+
+    # Display the plot
+    plt.show()
+
+
+def create_synthetic_model_prediction_plots():
+    # get data
+    data = np.array(load_data_from_csv(input_filepath='data.csv'))
+
+    # synthesis the models' predictions
+    base_model_predictions = synthesize_predictions_from_data(input_filepath='data.csv')
+
+    # plot
+    plot_data_vs_predictions(data=data, base_model_predictions=base_model_predictions)
 
 
 if __name__ == '__main__':
-    load_data_from_csv(input_filepath='data.csv', output_filepath='real_data/all_data.npz')
-    synthesize_predictions(input_filepath='real_data/all_data.npz')
+    # create_synthetic_model_prediction_plots()
+    create_prediction_samples_from_data(input_filepath='data.csv')
+
+    # create_samples_from_data(input_filepath='data.csv', output_filepath='inputs.npz')
+
